a138805c1b57d3d055728d3580b822f9c4f0047f
Sebastian Agudelo Marin <sebascarman@gmail.com>
2026-03-08 19:06:10 -0500

    feat: Initialize Quarkus REST application with Docker support
    - Added Maven Wrapper script for project setup.
    - Created pom.xml for managing project dependencies and build configuration.
    - Implemented Dockerfiles for different build types: JVM, legacy JAR, native, and micro.
    - Developed a simple REST endpoint in NumberResource.java returning a greeting message.
    - Added integration and unit tests for the REST endpoint.
    - Configured application properties file for future settings.

---
+++ vintageStore/rest-book/src/test/java/org/sebastian/quarkus/microservices/BookResourceIT.java
@@ -0,0 +1,8 @@
+package org.sebastian.quarkus.microservices;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class BookResourceIT extends BookResourceTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ vintageStore/rest-number/src/test/java/org/sebastian/quarkus/microservices/NumberResourceIT.java
@@ -0,0 +1,8 @@
+package org.sebastian.quarkus.microservices;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class NumberResourceIT extends NumberResourceTest {
+    // Execute the same tests but in packaged mode.
+}
