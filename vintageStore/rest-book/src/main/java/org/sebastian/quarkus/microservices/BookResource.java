9db6061dd104242fff8a4ed88256aa592e044008
Sebastian Agudelo Marin <sebascarman@gmail.com>
2026-03-10 18:56:59 -0500

    feat: Add fault tolerance to createABook method and implement fallback mechanism

--- vintageStore/rest-book/src/main/java/org/sebastian/quarkus/microservices/BookResource.java
+++ vintageStore/rest-book/src/main/java/org/sebastian/quarkus/microservices/BookResource.java
@@ -1,13 +1,18 @@
 package org.sebastian.quarkus.microservices;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.time.Instant;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.POST;
@@ -36,6 +41,8 @@ public class BookResource {
         summary = "Create a new book",
         description = "Creates a new book with the provided details and returns the created book."
     )
+    @Retry(maxRetries = 3, delay = 3000)
+    @Fallback(fallbackMethod = "fallingBackOncreatingABook")
     public Response createABook(@FormParam("title") String title, @FormParam("author") String author, @FormParam("yearOfPublication") int yearOfPublication, @FormParam("genre") String genre) {
         
         Book book = new Book();
@@ -51,5 +58,34 @@ public class BookResource {
             .status(Response.Status.CREATED)
             .entity(book)
             .build();
+    }
+
+    public Response fallingBackOncreatingABook(String title, String author, int yearOfPublication, String genre) throws FileNotFoundException {
+        
+        Book book = new Book();
+        book.isbn13 = "Will be generated when the service is back up";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();   
+        saveBookOnDisk(book);  
+           
+        logger.log(Level.WARNING, "Book created with fallback: {0}", book);
+
+        return Response
+            .status(Response.Status.PARTIAL_CONTENT)
+            .entity(book)
+            .build();
+    }
+
+    private void saveBookOnDisk(Book book) throws FileNotFoundException {
+        logger.log(Level.INFO, "Saving book to disk: {0}", book);
+        String bookJson = JsonbBuilder.create().toJson(book);
+        try (PrintWriter out = new PrintWriter("book-" + Instant.now().toEpochMilli() + ".json")) {
+            out.println(bookJson);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error saving book to disk", e);
+        }
     }
 }
--- vintageStore/rest-book/src/test/java/org/sebastian/quarkus/microservices/MockNumberProxy.java
+++ vintageStore/rest-book/src/test/java/org/sebastian/quarkus/microservices/MockNumberProxy.java
@@ -1,11 +1,17 @@
 package org.sebastian.quarkus.microservices;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import io.quarkus.test.Mock;
+
+@Mock
+@RestClient
 public class MockNumberProxy implements NumberProxy {
 
     @Override
     public IsbThirteen generateIsbNumbers() {
         IsbThirteen isbThirteen = new IsbThirteen();
-        isbThirteen.isbn13 = "13-mock";
+        isbThirteen.isbn13 = "13-1";
         return isbThirteen;
     }
     
